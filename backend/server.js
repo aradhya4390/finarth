be4ebe0956b64aa5a7ce10ef3062c6c4bb15cd4b
aradhya4390 <aradhyanamdev85@gmail.com>
2025-11-12 15:05:49 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,16 @@
-// server.js
-
-// Load environment variables
 require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-// Initialize app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -23,12 +18,10 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('✅ MongoDB connected successfully'))
 .catch((err) => console.error('❌ MongoDB connection error:', err));
 
-// Routes
 app.get('/', (req, res) => {
   res.send('🚀 Personal Finance Tracker API is running');
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${5001}`);
 });
